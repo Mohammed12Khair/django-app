67078b6678c47e85e0b917642038a3032a1c0ec8
Mohamed Khair <mohamed.khair@ericsson.com>
2024-12-07 13:19:11 +0400

    service

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -74,10 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DB_PATH=os.getenv("DB_PATH")
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DB_PATH,
         # 'NAME': '/home/ec2-user/efs/db.sqlite3',
     }
 }
